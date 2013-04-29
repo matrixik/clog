927b96cfbad8737cef3720e0d7f8e3198b379d83
Dobrosław Żybort <matrixik@gmail.com>
2013-04-30 00:39:41 +0200

    Update example

--- example_test.go
+++ example_test.go
@@ -11,6 +11,7 @@ var Log *clog.Clog = clog.NewClog()
 
 func Example() {
 	Log.AddOutput(os.Stdout, clog.LevelWarn)
+	Log.AddOutputRange(os.Stdout, clog.LevelDebug, clog.LevelInfo)
 	dailyFile := clog.NewDailyFile("/opt/logs/myprocess_%s.log")
 	Log.AddOutput(dailyFile, clog.LevelTrace)
 	Log.Info("Pi is %v", math.Pi)
